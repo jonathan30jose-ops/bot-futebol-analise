cf2a3343d0eec8971a7a723e9183172c539aceec
jonathan30jose-ops <jonathan30jose@gmail.com>
2026-06-30 15:47:50 -0300

    Update API_KEY to use API-Football

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ cred = credentials.Certificate("firebase_key.json")
 initialize_app(cred)
 
 # IMPORTANTE: Substitua o texto abaixo pela sua chave real da API-Football
-API_KEY = "SUA_CHAVE_AQUI"
+API_KEY = "API-Football"
 HEADERS = {'x-rapidapi-key': API_KEY, 'x-rapidapi-host': 'v3.football.api-sports.io'}
 
 def analisar_jogos_ao_vivo():
